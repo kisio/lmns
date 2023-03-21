f2ce0e06d0d0dcff8789bfdb4c8a13f4af967649
kisio <derrickkisio@gmail.com>
2023-03-21 09:11:56 +0300

    general styling

--- lime/resources/views/dashboard.blade.php
+++ lime/resources/views/dashboard.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 @section('content')
-    <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 text-white">
+    <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8 text-stone-900 ">
         <h1 class="text-2xl font-bold mb-4">Search Results</h1>
      @if(count($search_results ?? []) > 0)
             <h2 class="text-lg font-bold mb-2">Customers</h2>
         @if(!empty($search_results['customers']))
-            <table class="table-auto w-full">
+            <table class="table-auto w-full border-separate border border-slate-500">
                 <thead>
                     <tr>
-                        <th class="px-4 py-2">First Name</th>
-                        <th class="px-4 py-2">Last Name</th>
-                        <th class="px-4 py-2">Username</th>
+                        <th class="px-4 py-2 border-separate border border-slate-500">First Name</th>
+                        <th class="px-4 py-2 border-separate border border-slate-500">Last Name</th>
+                        <th class="px-4 py-2 border-separate border border-slate-500">Username</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($search_results['customers'] as $customer)
                         <tr>
-                            <td class="border px-4 py-2">{{ $customer->first_name }}</td>
-                            <td class="border px-4 py-2">{{ $customer->last_name }}</td>
-                            <td class="border px-4 py-2">{{$customer->username}}</td>
+                            <td class="border px-4 py-2 border-separate border border-slate-500">{{ $customer->first_name }}</td>
+                            <td class="border px-4 py-2 border-separate border border-slate-500">{{ $customer->last_name }}</td>
+                            <td class="border px-4 py-2 border-separate border border-slate-500">{{$customer->username}}</td>
                         </tr>
                     @endforeach
                 </tbody>
@@ -30,20 +30,20 @@
         @if(!empty($search_results['tickets']))
         <h2 class="text-lg font-bold mb-2">Tickets</h2>
         <div class="overflow-x-auto">
-            <table class="w-full table-auto">
+            <table class="w-full table-auto border-separate border border-slate-500">
                 <thead>
                     <tr>
-                        <th class="px-4 py-2">Title</th>
-                        <th class="px-4 py-2">Description</th>
-                        <th class="px-4 py-2">Username</th>
+                        <th class="px-4 py-2 border-separate border border-slate-500">Title</th>
+                        <th class="px-4 py-2 border-separate border border-slate-500">Description</th>
+                        <th class="px-4 py-2 border-separate border border-slate-500">Username</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($search_results['tickets'] as $ticket)
                         <tr>
-                            <td class="border px-4 py-2">{{ $ticket->title }}</td>
-                            <td class="border px-4 py-2">{{ $ticket->description }}</td>
-                            <td class="border px-4 py-2">{{ $ticket->username }}</td>
+                            <td class="border px-4 py-2 border-separate border border-slate-500">{{ $ticket->title }}</td>
+                            <td class="border px-4 py-2 border-separate border border-slate-500">{{ $ticket->description }}</td>
+                            <td class="border px-4 py-2 border-separate border border-slate-500">{{ $ticket->username }}</td>
                         </tr>
                     @endforeach
                 </tbody>
@@ -55,18 +55,18 @@
     @if(!empty($search_results['payments']))
     <h2 class="text-lg font-bold mb-2">Payments</h2>
     <div class="overflow-x-auto">
-        <table class="w-full table-auto">
+        <table class="w-full table-auto border-separate border border-slate-500">
             <thead>
                 <tr>
-                    <th class="px-4 py-2">Transaction Code</th>
-                    <th class="px-4 py-2">Amount</th>
+                    <th class="px-4 py-2 border-separate border border-slate-500">Transaction Code</th>
+                    <th class="px-4 py-2 border-separate border border-slate-500">Amount</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($search_results['payments'] as $payment)
                     <tr>
-                        <td class="border px-4 py-2">{{ $payment->transaction_code }}</td>
-                        <td class="border px-4 py-2">{{ $payment->amount }}</td>
+                        <td class="border px-4 py-2 border-separate border border-slate-500">{{ $payment->transaction_code }}</td>
+                        <td class="border px-4 py-2 border-separate border border-slate-500">{{ $payment->amount }}</td>
                     </tr>
                 @endforeach
             </tbody>
--- lime/resources/views/index.blade.php
+++ lime/resources/views/index.blade.php
@@ -1,27 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container flex justify-center items-center">
-    <div class="bg-gray-100 py-8 px-4 sm:px-6 lg:px-8 max-w-xl">
-        <div class="max-w-md mx-auto bg-white rounded-lg shadow-md overflow-hidden md:max-w-lg">
-          <div class="md:flex">
-            <div class="md:flex-shrink-0 bg-indigo-600">
-              <img class="h-48 w-full object-cover md:w-48" src="https://i.etsystatic.com/20986367/r/il/0bfeb2/2619062962/il_794xN.2619062962_lwdw.jpg" alt="Limesol-NMS Logo">
-            </div>
-            <div class="p-8">
-              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Limesol-NMS</div>
-              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">A Network Management System</a>
-              <p class="mt-2 text-gray-500">Limesol-NMS is a comprehensive Network Management System designed to help network administrators manage their network resources more efficiently. With advanced features such as real-time monitoring, performance analysis, and network automation, Limesol-NMS simplifies the complex task of managing a modern network.</p>
-              <div class="mt-4 flex justify-center">
-                @guest
-                <a href="{{ route('login') }}" class="inline-block bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded">Login</a>
-                <a href="{{ route('register') }}" class="inline-block bg-gray-400 hover:bg-gray-500 text-white py-2 px-4 rounded ml-2">Register</a>
-                @endguest
-                
-              </div>
-            </div>
+<div class="h-screen bg-gray-100 flex justify-center items-center">
+    <div class="w-full max-w-full lg:max-w-6xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+      <div class="md:flex">
+        <div class="md:flex-shrink-0 bg-indigo-600">
+          <img class="h-48 w-full object-cover md:w-48" src="https://i.etsystatic.com/20986367/r/il/0bfeb2/2619062962/il_794xN.2619062962_lwdw.jpg" alt="Limesol-NMS Logo">
+        </div>
+        <div class="p-8">
+          <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Limesol-NMS</div>
+          <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">A Network Management System</a>
+          <p class="mt-2 text-gray-500">Limesol-NMS is a comprehensive Network Management System designed to help network administrators manage their network resources more efficiently. With advanced features such as real-time monitoring, performance analysis, and network automation, Limesol-NMS simplifies the complex task of managing a modern network.</p>
+          <div class="mt-4 flex justify-center">
+            @guest
+            <a href="{{ route('login') }}" class="inline-block bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded">Login</a>
+            <a href="{{ route('register') }}" class="inline-block bg-gray-400 hover:bg-gray-500 text-white py-2 px-4 rounded ml-2">Register</a>
+            @endguest
+            
           </div>
         </div>
+      </div>
     </div>
 </div>
 @endsection
